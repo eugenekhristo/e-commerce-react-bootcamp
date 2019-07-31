bc3342896a163c0ba9aa3fbca578448e4d58abb8
Eugene Khristo <marteau9292@gmail.com>
2019-07-31 16:39:26 +0300

    MenuItem component is styled

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`menu-item ${size}`}
-  >
+  <div className={`menu-item ${size}`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className="background-image"
+    />
     <div className="content">
       <h1 className="title">{title}</h1>
       <p className="subtitle">shop now</p>
